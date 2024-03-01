bee52a5a3d94a49fe5662f7737e615b631b69fcb
MrSpokeMan <82821630+MrSpokeMan@users.noreply.github.com>
2024-03-01 08:17:52 +0100

    Add project files.

---
+++ FirstProject/FizzBuzz.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProject
+{
+    public class FizzBuzz
+    {
+        public static void Function()
+        {
+            Console.WriteLine("Enter a number as a upper bound: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < num; i++)
+            {
+                if(i % 3 == 0 && i % 5 == 0) Console.WriteLine("FizzBuzz");
+                else if(i % 3 == 0) Console.WriteLine("Fizz");
+                else if(i % 5 == 0) Console.WriteLine("Buzz");
+                else Console.WriteLine(i);
+            }
+        }
+    }
+}
